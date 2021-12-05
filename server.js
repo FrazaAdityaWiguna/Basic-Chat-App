b74b66c012c6bea34952fed39481e575aab5d76b
FrazaAdityaWiguna <bigdreamnums@gmail.com>
2021-12-05 11:28:54 +0700

    fix jumlah user

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-let countUserOnline = 1
+let countUserOnline = 0
 io.on('connection', (socket) => {
   socket.on('join', (params) => {
     console.log('User Join')
